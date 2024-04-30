2f479f633e52491ffa693c0eb36970b7647f9a29
Guilherme Teixeira <24235344+ggteixeira@users.noreply.github.com>
2024-04-29 22:33:41 -0300

    Kinda finishes the project

--- media.js
+++ media.js
@@ -1,9 +1,12 @@
 import { bring } from "./fetch.js"
 
-const HASH = 'djgzh5X6IpXg2lew9a7oo'
-const YEAR = 2018
-const URL = `https://www.olx.com.br/_next/data/${HASH}/pt-BR/autos-e-pecas/motos/honda/cb/250/${YEAR}/estado-sp.json?f=c&me=40000&route=motos&route=honda&route=cb&route=250&route=2018&route=estado-sp`
-const ARRAY = await bring(URL)
+const ARRAY2016 = await bring(2016)
+const ARRAY2017 = await bring(2017)
+const ARRAY2018 = await bring(2018)
+const ARRAY2019 = await bring(2019)
+const ARRAY2020 = await bring(2020)
+const ARRAY2021 = await bring(2021)
+const ARRAY2022 = await bring(2022)
 
 const meanCalculator = (array) => {
   const summation = array.reduce((previous, current) => {
@@ -19,31 +22,31 @@ const prettifier = (priceMean, pricesArray) => {
     currency: 'BRL'
   }).format(priceMean)
 
-  return `- Unidades disponíveis: ${pricesArray.length} \n- Média: ${formattedPrice}\n`
+  return `- Unidades à venda: ${pricesArray.length} \n- Média de preço: ${formattedPrice}\n`
 }
 
 console.log('Honda CB 250F Twister 2016: ')
-console.log(prettifier(meanCalculator(ARRAY), ARRAY))
+console.log(prettifier(meanCalculator(ARRAY2016), ARRAY2016))
 
-// console.log('Honda CB 250F Twister 2017:')
-// console.log(prettifier(meanCalculator(), prices2017))
-//
-// console.log('Honda CB 250F Twister 2018:')
-// console.log(prettifier(meanCalculator(prices2018), prices2018))
-//
-// console.log('Honda CB 250F Twister 2019:')
-// console.log(prettifier(meanCalculator(prices2019), prices2019))
-//
-// console.log('Honda CB 250F Twister 2020:')
-// console.log(prettifier(meanCalculator(prices2020), prices2020))
-//
-// console.log('Honda CB 250F Twister 2021:')
-// console.log(prettifier(meanCalculator(prices2021), prices2021))
-//
-// console.log('Honda CB 250F Twister 2022:')
-// console.log(prettifier(meanCalculator(prices2022), prices2022))
+console.log('Honda CB 250F Twister 2017:')
+console.log(prettifier(meanCalculator(ARRAY2017), ARRAY2017))
 
-// console.log('-------------------------------\n')
+console.log('Honda CB 250F Twister 2018:')
+console.log(prettifier(meanCalculator(ARRAY2018), ARRAY2018))
+
+console.log('Honda CB 250F Twister 2019:')
+console.log(prettifier(meanCalculator(ARRAY2019), ARRAY2019))
+
+console.log('Honda CB 250F Twister 2020:')
+console.log(prettifier(meanCalculator(ARRAY2020), ARRAY2020))
+
+console.log('Honda CB 250F Twister 2021:')
+console.log(prettifier(meanCalculator(ARRAY2021), ARRAY2021))
+
+console.log('Honda CB 250F Twister 2022:')
+console.log(prettifier(meanCalculator(ARRAY2022), ARRAY2022))
+
+console.log('-------------------------------\n')
 const entrada = 8000
 
 const divisor = (price, entrada) => {
@@ -63,13 +66,11 @@ const divisor = (price, entrada) => {
   return `(R$ ${formattedPrice} - R$ ${entrada}) / 12 = ${formattedDivision}\n`
 }
 
-// console.log(`Simulador de parcelamento 2016: \n${divisor(meanCalculator(bring(URL)), entrada)}`)
 
-// console.log(`Simulador de parcelamento 2016: \n${divisor(meanCalculator(prices2016), entrada)}`)
-// console.log(`Simulador de parcelamento 2017: \n${divisor(meanCalculator(prices2017), entrada)}`)
-// console.log(`Simulador de parcelamento 2018: \n${divisor(meanCalculator(prices2018), entrada)}`)
-// console.log(`Simulador de parcelamento 2019: \n${divisor(meanCalculator(prices2019), entrada)}`)
-// console.log(`Simulador de parcelamento 2020: \n${divisor(meanCalculator(prices2020), entrada)}`)
-// console.log(`Simulador de parcelamento 2021: \n${divisor(meanCalculator(prices2021), entrada)}`)
-// console.log(`Simulador de parcelamento 2022: \n${divisor(meanCalculator(prices2022), entrada)}`)
-//
+console.log(`Simulador de parcelamento 2016: \n${divisor(meanCalculator(ARRAY2016), entrada)}`)
+console.log(`Simulador de parcelamento 2017: \n${divisor(meanCalculator(ARRAY2017), entrada)}`)
+console.log(`Simulador de parcelamento 2018: \n${divisor(meanCalculator(ARRAY2018), entrada)}`)
+console.log(`Simulador de parcelamento 2019: \n${divisor(meanCalculator(ARRAY2019), entrada)}`)
+console.log(`Simulador de parcelamento 2020: \n${divisor(meanCalculator(ARRAY2020), entrada)}`)
+console.log(`Simulador de parcelamento 2021: \n${divisor(meanCalculator(ARRAY2021), entrada)}`)
+console.log(`Simulador de parcelamento 2022: \n${divisor(meanCalculator(ARRAY2022), entrada)}`)
